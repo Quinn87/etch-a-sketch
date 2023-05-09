0d2e71335212c2ff6de207f9cf6f211b44861749
Brandon Quinn <github@thequinn.org>
2023-05-09 09:34:53 -0500

    Cleaning up; Added gridSize variable

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const container = document.querySelector('#content');
 
-for (i = 0; i < 16; i++) {
+let gridsize = 16
+
+for (i = 0; i < gridsize; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
 
-    for (j = 0; j < 16; j++){
+    for (j = 0; j < gridsize; j++){
         const nextDiv = document.createElement('div');
-        // nextDiv.style.border = '1px solid gray';
         nextDiv.style.padding = '10px';
         nextDiv.style.width = '5px';
         nextDiv.style.height = '5px';
         nextDiv.classList.add('cells')
         div.appendChild(nextDiv);
-        console.log(nextDiv);   
     }
 }
