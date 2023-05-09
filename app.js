44daae0d7ba4d0be9b7f4853fabf1de3a1b8d1db
Brandon Quinn <github@thequinn.org>
2023-05-09 11:16:05 -0500

    Removed cell width and height

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('#content');
-
+const gridCells = document.querySelectorAll('.cells');
 let gridsize = 16
 
 for (i = 0; i < gridsize; i++) {
@@ -9,9 +9,9 @@ for (i = 0; i < gridsize; i++) {
     for (j = 0; j < gridsize; j++){
         const nextDiv = document.createElement('div');
         nextDiv.style.padding = '10px';
-        nextDiv.style.width = '5px';
-        nextDiv.style.height = '5px';
         nextDiv.classList.add('cells')
         div.appendChild(nextDiv);
     }
 }
+
+foreach.gridCells()
