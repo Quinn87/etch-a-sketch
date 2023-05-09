6d6a525ceacdc10cae6c60e3a9d03e879d2182d1
Brandon Quinn <github@thequinn.org>
2023-05-09 15:59:37 -0500

    cellSize is now scaling to gridSize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector('#grid');
 
-let gridsize = 16
+let gridsize = 16;
+let cellSize = 600 / gridsize;
 
 for (i = 0; i < gridsize; i++) {
     const div = document.createElement('div');
@@ -8,7 +9,8 @@ for (i = 0; i < gridsize; i++) {
 
     for (j = 0; j < gridsize; j++){
         const nextDiv = document.createElement('div');
-        nextDiv.style.padding = '10px';
+        nextDiv.style.height = cellSize + "px";
+        nextDiv.style.width = cellSize + "px";
         nextDiv.classList.add('cells')
         div.appendChild(nextDiv);
     }
