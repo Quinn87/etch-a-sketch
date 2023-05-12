0be90115027a554e650ad0a86ac7b7730afab766
Brandon Quinn <github@thequinn.org>
2023-05-12 15:01:49 -0500

    Grid for loop more efficient

--- app.js
+++ app.js
@@ -4,25 +4,18 @@ const gridSizeRange = document.querySelector('#gridSizeRange')
 const erase = document.querySelector('.erase')
 const draw = document.querySelector('.draw')
 
-let gridSize = 16;
+let gridSize = 70;
 let cellSize = 600 / gridSize;
 
-for (i = 0; i < gridSize; i++) {
-    const firstDiv = document.createElement('div');
-    firstDiv.classList.add('firstDiv')
-    grid.appendChild(firstDiv);
-
-    for (j = 0; j < gridSize; j++){
-        const nextDiv = document.createElement('div');
-        nextDiv.style.height = cellSize + "px";
-        nextDiv.style.width = cellSize + "px";
-        nextDiv.classList.add('cells')
-        firstDiv.appendChild(nextDiv);
-    }
+for (i = 0; i < Math.pow(gridSize, 2)  ; i++) {
+    const cell = document.createElement('div');
+    cell.style.height = cellSize + "px";
+    cell.style.width = cellSize + "px";
+    cell.classList.add('cells')
+    grid.appendChild(cell);
 }
 
 const gridCells = document.querySelectorAll('.cells');
-const firstDivs = document.querySelectorAll('.firstDiv');
 
 draw.addEventListener('click', function (){
     gridCells.forEach(cell => cell.addEventListener ('mousemove', function(){
